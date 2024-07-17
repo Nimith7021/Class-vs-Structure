19ed5fce6df4513e103ba3a21cf0dcf0819668c7
Nimith Gangadhar Shetty <Nimith.Shetty@asl.mum.sst>
2024-07-17 18:53:58 +0530

    initial commit

---
+++ Models/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalClass.Models
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+
+        public bool IsRare { get; set; }
+
+        public string Color { get; set; }
+
+        public Animal(string name, bool isRare, string color)
+        {
+            Name = name;
+            IsRare = isRare;
+            Color = color;
+        }
+
+        public override string ToString()
+        {
+            return $"The name of the animal is {Name}\n" +
+                $"Is the animal rare ? {IsRare}\n" +
+                $"The color of the animal : {Color}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using AnimalClass.Models;
+
+namespace AnimalClass
+{
+    internal class Program
+    {
+        public struct Bird
+        {
+            public string Name;
+            public bool isRare;
+            public string Color;
+        }
+        static void Main(string[] args)
+        {
+            Bird bird;
+            bird.Name = "Crow";
+            bird.isRare = false;
+            bird.Color = "Black";
+
+            Bird bird1 = bird;
+            Console.WriteLine("======================FOR STRUCTURE=====================");
+            Console.WriteLine("The name of the bird is : " + bird.Name);
+            Console.WriteLine("Is the bird rare ? : " + bird.isRare);
+            Console.WriteLine("The color of the bird is : " + bird.Color);
+
+            bird.Name = "Cat";
+
+            Console.WriteLine("\n");
+            Console.WriteLine("The value of bird name in bird " + bird.Name); //it is value based 
+            Console.WriteLine("The value of bird name in bird1 " + bird1.Name);
+
+
+
+            Console.WriteLine("=========================FOR CLASS======================");
+            Animal animal = new Animal("Dog", false, "Brown");
+            Animal animal1 = animal;
+
+            Console.WriteLine("========for animal object========");
+            Console.WriteLine(animal);
+            Console.WriteLine("========for animal1 object=======");
+            Console.WriteLine(animal1);
+
+            animal.Name = "Cat";
+
+            Console.WriteLine("========for animal object========"); /*changes are reflected on both 
+                                                                     * objects since its referential*/
+            Console.WriteLine("The name of the animal is "+animal.Name);
+            Console.WriteLine("========for animal1 object=======");
+            Console.WriteLine("The name of the animal is " +animal1.Name);
+
+
+        }
+    }
+}
